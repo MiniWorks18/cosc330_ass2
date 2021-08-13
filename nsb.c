0fb8fc899c273ff4d65495e94bbc630fbceeacf9
Tully McDonald <tmcdon26@turing.une.edu.au>
2021-08-13 12:47:51 +1000

    commenting and final touches

--- nsb.c
+++ nsb.c
@@ -1,5 +1,4 @@
 // Nordvik Suspension Bridge solution using threads and semaphores
-
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -19,73 +18,60 @@
 int e_counter, w_counter = 0; // Initialise counters
 int e_sem, w_sem, bridge_sem; // Declare semaphores
 
+/* Struct of a Vehicle and the properties it can hold
+    int counter - The counter integer for how many vehicles are on the bridge
+    int semaphore - The mutex semaphore providing exclusive access to 
+                    the counter
+    int id - The id of this particular car
+    car dir - The direction that the car is travelling (e.g 'E' or 'W')
+*/
 typedef struct
 {
     int *counter, semaphore, id;
     char dir;
 } Vehicle;
 
-// Entry point for east bound car threads
-void *eastcar(void *arg)
+void *carfunc(void *arg) // Entry point for east bound car threads
 {
-    /* 
-Car function should act similarly to the reader function
-- Lock EC
-- Increment EC
-- If EC == 1
-    - Lock bridge
-- Unlock EC
-- Drive (sleep(DRIVE_TIME))
-- Lock EC
-- Decrement EC
-- If EC == 0
-    - Unlock bridge
-- Unlock EC
-*/
-
-    sleep(rand() % MAXWAIT);
+    sleep(rand() % MAXWAIT); // Delay rand amount of time
 
     Vehicle c = *((Vehicle *)arg);
     int sem = c.semaphore;
     int *counter = c.counter;
 
-    P(sem);
+    P(sem); // Lock counter
     *counter += 1;
-    if (*counter == 1)
+    if (*counter == 1) // Lock bridge if first car
         P(bridge_sem);
-    printf("Hi, I'm car %d entering the bridge heading %c. Beep Beep!\n", c.id, c.dir);
-    V(sem);
-    sleep(CROSSINGTIME);
-    P(sem);
+    printf("Hi, I'm car %d entering the bridge heading %c. Beep Beep!\n",
+           c.id, c.dir);
+    V(sem);              // Unlock counter
+    sleep(CROSSINGTIME); // Drive
+    P(sem);              // Lock counter
     *counter -= 1;
-    printf("Ok, I'm car %d exiting the bridge heading %c. Thanks!\n", c.id, c.dir);
-    if (*counter == 0)
+    printf("Ok, I'm car %d exiting the bridge heading %c. Thanks!\n",
+           c.id, c.dir);
+    if (*counter == 0) // Unlock bridge if last car
         V(bridge_sem);
     V(sem);
     pthread_exit(NULL);
 }
 
-// Entry point for truck threads
-void *truckfunc(void *arg)
+void *truckfunc(void *arg) // Entry point for truck threads
 {
-    /*
-- Lock bridge
-- Drive
-- Unlock bridge
-*/
     sleep(rand() % MAXWAIT);
-    Vehicle t = *((Vehicle *)arg);
-    // TODO: Implment this function
-    P(bridge_sem);
-    printf("Hi, I'm truck %d enering the bridge heading %c.\n", t.id, t.dir);
-    sleep(CROSSINGTIME);
-    printf("Ok, I'm truck %d exiting the bridge heading %c.\n", t.id, t.dir);
-    V(bridge_sem);
 
+    Vehicle t = *((Vehicle *)arg);
+
+    P(bridge_sem); // Lock bridge for exclusive access
+    printf("Hi, I'm truck %d enering the bridge heading %c.\n", t.id, t.dir);
+    sleep(CROSSINGTIME); // Drive
+    printf("Ok, I'm truck %d exiting the bridge heading %c.\n", t.id, t.dir);
+    V(bridge_sem); // Unlock bridge
     pthread_exit(NULL);
 }
 
-void cleanup(void)
+void cleanup(void) // Remove semaphores after completion
 {
     rm_sem(e_sem);
     rm_sem(w_sem);
@@ -95,15 +81,10 @@ void cleanup(void)
 int main(void)
 {
     int i;
-    // pthread_t e_tid[E_CARS];
-    // pthread_t w_tid[W_CARS];
     pthread_t tid[E_CARS + W_CARS];
     pthread_t ttruckid[E_TRUCKS + W_TRUCKS];
-    // int truckid[E_TRUCKS + W_TRUCKS];
 
-    // Car e_cars[E_CARS];
-    // Car w_cars[W_CARS];
-
+    // Vehcile array declaration
     Vehicle cars[E_CARS + W_CARS];
     Vehicle trucks[E_TRUCKS + W_TRUCKS];
 
@@ -111,79 +92,17 @@ int main(void)
     e_sem = semtran(IPC_PRIVATE);
     w_sem = semtran(IPC_PRIVATE);
     bridge_sem = semtran(IPC_PRIVATE);
+
     // Initialise to 1 (binary semaphores)
     V(e_sem);
     V(w_sem);
     V(bridge_sem);
 
-    // TODO: Figure out why this doesn't let me use cars[] why it gives segmentation faults
-
-    // for (i = 0; i < E_CARS; i++)
-    // {
-    //     e_cars[i].id = i;
-    //     e_cars[i].dir = 'E';
-    //     e_cars[i].counter = &e_counter;
-    //     e_cars[i].semaphore = e_sem;
-    // }
-    // for (i = 0; i < W_CARS; i++)
-    // {
-    //     // if (i >= E_CARS)
-    //     // {
-    //     w_cars[i].id = i;
-    //     w_cars[i].dir = 'W';
-    //     w_cars[i].counter = &w_counter;
-    //     w_cars[i].semaphore = w_sem;
-    //     // }
-    //     // else
-    //     // {
-    //     // }
-    // }
-    // for (i = 0; i < TRUCKS; i++)
-    //     truckid[i] = i;
-
-    // for (i = 0; i < E_CARS; i++)
-    //     pthread_create(&e_tid[i], NULL, eastcar, (void *)&e_cars[i]);
-
-    // for (i = 0; i < W_CARS; i++)
-    // {
-    //     // if (i >= E_CARS)
-    //     // {
-    //     pthread_create(&w_tid[i], NULL, eastcar, (void *)&w_cars[i]);
-    //     // }
-    //     // else
-    //     // {
-    //     // pthread_create(&e_tid[i], NULL, eastcar, (void *)&cars[i]);
-    //     // }
-    // }
-    // for (i = 0; i < TRUCKS; i++)
-    //     pthread_create(&ttruckid[i], NULL, truckfunc, (void *)&truckid[i]);
-
-    // for (i = 0; i < E_CARS; i++)
-    //     pthread_join(e_tid[i], NULL);
-
-    // for (i = 0; i < W_CARS; i++)
-    // {
-    //     // if (i >= E_CARS)
-    //     // {
-    //     pthread_join(w_tid[i], NULL);
-    //     // }
-    //     // else
-    //     // {
-    //     //     pthread_join(e_tid[i], NULL);
-    //     // }
-    // }
-
-    // for (i = 0; i < E_CARS; i++)
-    // {
-    //     cars[i].id = i;
-    //     cars[i].dir = 'E';
-    //     cars[i].counter = &e_counter;
-    //     cars[i].semaphore = e_sem;
-    // }
+    // Loop for each vehicle, initialise properties
     for (i = 0; i < E_CARS + W_CARS; i++)
     {
         cars[i].id = i;
-        if (i >= E_CARS)
+        if (i >= E_CARS) // If cars above the E_CARS index
         {
             cars[i].dir = 'W';
             cars[i].counter = &w_counter;
@@ -205,16 +124,18 @@ int main(void)
             trucks[i].dir = 'E';
     }
 
+    // Create pthreads for each vehicle
     for (i = 0; i < E_CARS + W_CARS; i++)
     {
         if (i >= E_CARS)
-            pthread_create(&tid[i], NULL, eastcar, (void *)&cars[i]);
+            pthread_create(&tid[i], NULL, carfunc, (void *)&cars[i]);
         else
-            pthread_create(&tid[i], NULL, eastcar, (void *)&cars[i]);
+            pthread_create(&tid[i], NULL, carfunc, (void *)&cars[i]);
     }
     for (i = 0; i < E_TRUCKS + W_TRUCKS; i++)
         pthread_create(&ttruckid[i], NULL, truckfunc, (void *)&trucks[i]);
 
+    // Join to each thread and wait until each have exited
     for (i = 0; i < E_CARS + W_CARS; i++)
     {
         if (i >= E_CARS)
@@ -222,9 +143,10 @@ int main(void)
         else
             pthread_join(tid[i], NULL);
     }
-
     for (i = 0; i < E_TRUCKS + W_TRUCKS; i++)
         pthread_join(ttruckid[i], NULL);
+
+    // Cleanup semaphores and exit
     cleanup();
     return 0;
 }
